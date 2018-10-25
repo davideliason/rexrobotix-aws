270629f2d6937fde12861c72564ccbd1a0880c63
David Eliason <davethemaker@gmail.com>
2018-10-24 18:29:43 -0700

    mongodb connection establish

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 const express = require('express');
 const path = require('path');
+const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+
 require('dotenv').config()
 
 const app = express();
+const port = process.env.PORT || 8080;
+const test = process.env.TEST;
+const uri = process.env.DB_MLAB;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/api/users', (req,res) => {
-    res.json([{
-        id: 1,
-        username: "David"
-    }, {
-        id: 2,
-        username: "Johnny"
-    }]);
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, database) => {
+    console.log("db connection");
+
+    app.get('/api/users', (req,res) => {
+        res.json([{
+            id: 1,
+            username: "David"
+        }, {
+            id: 2,
+            username: "Johnny"
+        }]);
+    });
+
+    // Catchall handler for any other requests --> CRA index.html
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    });
+
 });
 
-// Catchall handler for any other requests --> CRA index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
 
-  const port = process.env.PORT || 8080;
-  const test = process.env.TEST;
+
   app.listen(port);
   
   console.log(`testing one two three ${test}`)
